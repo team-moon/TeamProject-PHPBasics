798b05c98d183473167d07b596661ec50bfe4ff7
AlexKondov <alex.kondov@abv.bg>
2014-08-17 15:03:37 +0300

    test theme and layout

---
+++ test-theme/index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Home</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<div id="logo">
+				Team Moon's Awesome Forum
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Forum</a></li>
+					<li><a href="#">Gallery</a></li>
+					<li><a href="#">Contacts</a></li>
+				</ul>
+				
+			</nav>
+		</header>
+
+		<div id="asides">
+			<aside>
+				<header>Log In</header>
+				<div id="log_in">
+						<div>Sign in:</div>
+						<form action="" method="post">
+							<input type="text" name="user_name" placeholder="User Name...">
+							<br>
+							<input type="password" name="password" placeholder="Password...">
+							<br>
+							<input type="submit" value="Submit">
+						</form>
+						<br>
+						<a href="#">Don't have an account? Sign up!</a>
+				</div>
+			</aside>
+			
+			<aside>
+				<header>Our Friends</header>
+				<div id="friends_links">
+					<ul>
+						<li><a href="#">Sofware University</a></li>
+						<li><a href="">TBD</a></li>
+						<li><a href="">TBD</a></li>
+						<li><a href="">TBD</a></li>
+						<li><a href="">TBD</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+		<main>
+			<section>
+					<header>Hello!</header>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+			</section>
+			<section>
+					<header>Hello!</header>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+			</section>
+			<section>
+					<header>Hello!</header>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+					<article>
+						<header>Article Header</header>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat modi fugit dolore libero, voluptas cum, 
+							qui a et eos minus enim magnam 
+							consectetur veritatis ab mollitia molestiae iure beatae tenetur?
+						</p>
+					</article>
+			</section>
+			
+			
+		</main>
+
+		<footer>
+			<div id="footer-site">
+				Team Moon 2014&copy;. All Rights Reserved.
+			</div>
+			<div class="social">
+				<ul>
+					<li><a href="#" id="facebook"></a></li>
+					<li><a href="#" id="twitter"></a></li>
+					<li><a href="#" id="instagram"></a></li>
+					<li><a href="#" id="linkedin"></a></li>
+					<li><a href="#" id="flickr"></a></li>
+				</ul>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
