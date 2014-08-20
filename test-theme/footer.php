92ec076645a1eaa0303eefd1c5360fb8250b2ec6
AlexKondov <alex.kondov@abv.bg>
2014-08-20 22:02:53 +0300

    updt theme

---
+++ test-theme/footer.php
@@ -0,0 +1,19 @@
+		</main>
+
+		<footer>
+			<div id="footer-site">
+				Team Moon 2014&copy;. All Rights Reserved.
+			</div>
+			<div class="social">
+				<ul>
+					<li><a href="#" id="facebook"></a></li>
+					<li><a href="#" id="twitter"></a></li>
+					<li><a href="#" id="instagram"></a></li>
+					<li><a href="#" id="linkedin"></a></li>
+					<li><a href="#" id="flickr"></a></li>
+				</ul>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>
---
+++ test-theme/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="script.js"></script>
+</head>
+<body>
+	<div id="wrapper">
+		<header>
+			<div id="logo">
+				Team Moon's Awesome Forum
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="posts.php">Forum</a></li>
+					<li><a href="#">Contacts</a></li>
+				</ul>
+				
+			</nav>
+		</header>
