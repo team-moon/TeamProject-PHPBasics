bdfb8713c47d355fd86fe85236fd629025d10aed
Antoveravip <petar@petarivanov.com>
2014-08-23 17:09:27 +0300

    adniministration php
    Refactoring and update administration page

--- administration.php
+++ administration.php
@@ -20,64 +20,40 @@ $pageTitle = 'Administration';
 require 'includes/header.php';
 ?>
 
-<h2>Administrate Users</h2>
-
+<h2>Administration page</h2>
 <div id="admin-form">
-    <form method="POST" action="processing/manage-administration.php" role="form">
-        <div id="choose-user">
-            <?php $users = getAllUsers($connection, false); ?>
-            <select name="user">
-                <option value="choose user">Choose User</option>
-
-                <?php
-                for ($i = 0; $i < count($users['user_id']); $i++) {
-                    echo '<option value="' . $users['user_id'][$i] . '">' . $users['name'][$i] . '</option>';
-                }
-                ?>                
-            </select>
-            <input type="submit" name="show-user" value="Show" /> or
-            <input type="submit" name="delete-user" value="Delete" />           
-        </div><!-- #choose-user -->
-
+    <div id="user-administration">
+        <h3>Users administration</h3>
         <?php
-        if (isset($_GET['id']) && (int) $_GET['id'] > 0 && (int) $_GET['id'] != $_SESSION['userId'] &&
-            getUsernameById($connection, $_GET['id']) &&
-            isset($_GET['level']) && $_GET['level'] > 0) {
-            
-            $id = (int) $_GET['id'];
-            $level = (int) $_GET['level'];
-            
-            $username = getUsernameById($connection, $id);
-         ?>
-        
-            <div id="change-user-access">
-                <p>Change the access level of user: <span><?php echo $username; ?></span></p>
-                <p>
-                    <?php
-                    $accessLevels = getAllAccessLevels($connection);
-
-                    for ($i = 0; $i < count($accessLevels['access_lvl']); $i++) {
-                        $accessLevelName = $accessLevels['access_name'][$i];
-                        $accessLevelId = $accessLevels['access_lvl'][$i];
-                        
-                        if ($level == $accessLevelId) {
-                            $checked = 'checked';
-                        } else {
-                            $checked = '';
-                        }
-
-                        echo '<input type="radio" name="access-level-id" value="' . $accessLevelId . '" id="' . $accessLevelName . '" ' . $checked . ' />';
-                        echo '<label for="' . $accessLevelName . '">' . $accessLevelName . '</label>';
-                    }
-                    ?>
-                    <input type="hidden" name="id" value="<?php echo $id; ?>" />
-                    <input type="submit" name="change-user-access" value="Change" />
-                </p>
-            </div><!-- #change-user-access -->
-            
-        <?php } ?>
-    </form>
+            require 'admin/admin-user.php';
+        ?>
+    </div>
+    <div id="post-administration">
+        <h3>Posts administration</h3>
+        <?php
+            require 'admin/admin-post.php';
+        ?>
+    </div>
+    <div id="comments-administration">
+        <h3>Comments administration</h3>
+        <?php
+            require 'admin/admin-comment.php';
+        ?>
+    </div>
+    <div id="categories-administration">
+        <h3>Categories administration</h3>
+        <?php
+            require 'admin/admin-category.php';
+        ?>
+    </div>
 </div><!-- #admin-form -->
 
 <?php
+unset($_SESSION['temp-post-title']);
+unset($_SESSION['temp-post-datePublished']);
+unset($_SESSION['temp-post-tags']);
+unset($_SESSION['temp-post-text']);
+unset($_SESSION['temp-post-categoryId']);
+unset($_SESSION['temp-category-name']);
+
 require 'includes/footer.php';
