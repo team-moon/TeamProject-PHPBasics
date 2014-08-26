c9ba1a64375106bf4fea72aec669a0306c8ffb4d
Nikolay Chochev <nikygat@mail.bg>
2014-08-26 13:23:13 +0300

    design fix

--- login.php
+++ login.php
@@ -23,9 +23,17 @@ if (isset($_SESSION['temp-username'])) {
 <h2><?php echo $pageTitle; ?></h2>
 
 		<form method="POST" action="processing/check-user.php" role="form">
-			<input type="text" name="username" placeholder="username" placeholder="username" required autocomplete="on" />
-            <input type="password" name="password" placeholder="password" placeholder="password" required />
-            <input type="submit" name="user-action" value="Login" />
+			<p>
+				<label for="username">Username: </label>
+				<input id="username" type="text" name="username" required autocomplete="off" value="<?php echo $username; ?>" />
+			</p>
+			<p>
+				<label for="password">Password: </label>
+				<input id="password" type="password" name="password" required />
+			</p>
+			<p>
+				<input type="submit" name="user-action" value="Login" />
+			</p>
 		</form>
 </div>
 
